61f7d0ef06caede6570b43fdbc874dcd38d8e4a0
Will Riley <will@hflw.org>
2016-03-04 21:51:12 -0800

    Moved quad generation into separate file + wrote implementation

--- lib/formless/store.ex
+++ lib/formless/store.ex
@@ -1,31 +1,11 @@
 defmodule Formless.Store do
   alias Formless.Store.Analysis
+  alias Formless.Store.Quads
 
   def write(bucket, text) do
     text
     |> Analysis.ngrams()
-    |> Enum.each(&write_quads(bucket, &1))
+    |> Enum.each(&Quads.write(bucket, &1))
   end
 
-  defp write_quads(bucket, ngram) do
-    Enum.each [:begins_with, :ends_with], fn(predicate) ->
-      ngram
-      |> split_to_quads(predicate)
-      |> Enum.each(&write_quad(bucket, ngram, predicate, &1))
-    end
-  end
-
-  defp split_to_quads(ngram, predicate) when predicate in [:begins_with, :ends_with] do
-    #TODO
-  end
-
-  defp write_quad(bucket, ngram, predicate, words) when predicate in [:begins_with, :ends_with] do
-    # TODO: should we use `length(ngram)` in the label/predicate so we can only query ngrams of length n?
-    Formless.Graph.write(%{
-      subject: Enum.join(ngram, " "),
-      predicate: Atom.to_string(predicate),
-      object: Enum.join(words, " "),
-      label: bucket
-    })
-  end
 end
---
+++ lib/formless/store/quads.ex
@@ -0,0 +1,30 @@
+defmodule Formless.Store.Quads do
+  def write(bucket, ngram) do
+    Enum.each [:begins_with, :ends_with], fn(predicate) ->
+      ngram
+      |> split_to_quads(predicate)
+      |> Enum.each(&write_quad(bucket, ngram, predicate, &1))
+    end
+  end
+
+  defp split_to_quads([], _) do
+    []
+  end
+  defp split_to_quads(ngram, predicate) when predicate in [:begins_with, :ends_with] do
+    drop_from = case predicate do
+      :ends_with -> 1
+      :begins_with -> -1
+    end
+    [ngram] ++ split_to_quads(Enum.drop(ngram, drop_from), predicate)
+  end
+
+  defp write_quad(bucket, ngram, predicate, words) when predicate in [:begins_with, :ends_with] do
+    # TODO: should we use `length(ngram)` in the label/predicate so we can only query ngrams of length n?
+    Formless.Graph.write(%{
+      subject: Enum.join(ngram, " "),
+      predicate: Atom.to_string(predicate),
+      object: Enum.join(words, " "),
+      label: bucket
+    })
+  end
+end

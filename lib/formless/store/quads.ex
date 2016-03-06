4eacdf4453bac230f6d17073fd06613a55a505bb
0chroma <21014+0chroma@users.noreply.github.com>
2016-03-05 16:29:08 -0800

    Got quad writer working, started experimenting with queries in gremlin, built latest cayley docker image

--- lib/formless/store/quads.ex
+++ lib/formless/store/quads.ex
@@ -10,19 +10,19 @@ defmodule Formless.Store.Quads do
   defp split_to_quads([], _) do
     []
   end
-  defp split_to_quads(ngram, predicate) when predicate in [:begins_with, :ends_with] do
+  defp split_to_quads(ngram, predicate) do
     drop_from = case predicate do
       :ends_with -> 1
       :begins_with -> -1
     end
-    [ngram] ++ split_to_quads(Enum.drop(ngram, drop_from), predicate)
+    [Enum.drop(ngram, drop_from)] ++ split_to_quads(Enum.drop(ngram, drop_from*2), predicate)
   end
 
   defp write_quad(bucket, ngram, predicate, words) when predicate in [:begins_with, :ends_with] do
     # TODO: should we use `length(ngram)` in the label/predicate so we can only query ngrams of length n?
     Formless.Graph.write(%{
       subject: Enum.join(ngram, " "),
-      predicate: Atom.to_string(predicate),
+      predicate: "#{Atom.to_string(predicate)}_#{length(words)}",
       object: Enum.join(words, " "),
       label: bucket
     })

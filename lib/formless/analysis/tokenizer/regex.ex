97235c3f01748b121547efcf307a87014d33002e
Will Riley <will@hflw.org>
2016-03-25 23:49:05 -0700

    Made regex tokenizer handle parens better

--- lib/formless/analysis/tokenizer/regex.ex
+++ lib/formless/analysis/tokenizer/regex.ex
@@ -1,7 +1,7 @@
 defmodule Formless.Analysis.Tokenizer.Regex do
   @behaviour Formless.Analysis.Tokenizer
 
-  @token_pattern ~r/\s|(?<=\p{Po})|(?=\p{Po})/u
+  @token_pattern ~r/\s|(?<=(\p{Po}|\p{Ps}))|(?=(\p{Po}|\p{Pe}))/u
   def tokenize(text, opts \\ []) do
     regex = Keyword.get(opts, :regex, @token_pattern)
     String.split(text, regex, trim: true)

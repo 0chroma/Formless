a4551a95f553807545ea20124af8a915b25f38ba
0chroma <21014+0chroma@users.noreply.github.com>
2016-03-25 23:01:38 -0700

    Implemented more token analysis functions

--- test/formless/analysis/tokens_test.exs
+++ test/formless/analysis/tokens_test.exs
@@ -3,22 +3,40 @@ defmodule Formless.Analysis.TokensTest do
 
   alias Formless.Analysis.Tokens
 
-  test "from text" do
-    assert Tokens.from_text("Hello! This is a \"string\".") == ["Hello", "!", "This", "is", "a", "\"", "string", "\"", "."]
-    assert Tokens.from_text("Here's (some parenthesis) to test.") == ["Here's", "(", "some", "parenthesis", ")", "to", "test", "."]
+  test "words from text" do
+    assert Tokens.words_from_text("Hello! This is a \"string\".") == ["Hello", "!", "This", "is", "a", "\"", "string", "\"", "."]
+    assert Tokens.words_from_text("Here's (some parenthesis) to test.") == ["Here's", "(", "some", "parenthesis", ")", "to", "test", "."]
   end
 
-  test "group by sentence" do
-    tokens = [
-      "This", ",", "is", "a", "\"", "test", "\"", "sentence", ".",
-      "It", "has", "(", "lots", "of", ")", "punctuation", ",", "but", "{", "only", "}",
-      "splits", "on", "\"", ".", "\"", "characters", "."
-    ]
+  test "sentences from text" do
+    input = "Hans Rudolf \"Ruedi\" Giger (/ˈɡiːɡər/ ghee-gur; German: [ˈɡiːɡər]; 5 February 1940 – 12 May 2014) was a Swiss surrealist painter, whose style was adapted for many media, including record-albums, furniture and tattoo-art. His paintings are on display at the H.R. Giger Museum at Gruyères."
     output = [
-      ["This", ",", "is", "a", "\"", "test", "\"", "sentence", "."],
-      ["It", "has", "(", "lots", "of", ")", "punctuation", ",", "but", "{", "only", "}",
-      "splits", "on", "\"", ".", "\"", "characters", "."]
+      "Hans Rudolf \"Ruedi\" Giger (/ˈɡiːɡər/ ghee-gur; German: [ˈɡiːɡər]; 5 February 1940 – 12 May 2014) was a Swiss surrealist painter, whose style was adapted for many media, including record-albums, furniture and tattoo-art.",
+      "His paintings are on display at the H.R. Giger Museum at Gruyères."
     ]
-    assert Tokens.group_by_sentence(tokens) == output
+    assert Tokens.sentences_from_text(input) == output
+  end
+
+  test "edge shingles" do
+    output = [
+      ["Test", "sentence", "please", "ignore"],
+      ["Test", "sentence", "please"],
+      ["Test", "sentence"],
+      ["Test"],
+      ["sentence", "please", "ignore", "."],
+      ["please", "ignore", "."],
+      ["ignore", "."],
+      ["."]
+    ]
+    assert Tokens.edge_shingles(["Test", "sentence", "please", "ignore", "."]) == output
+    assert Tokens.edge_shingles(["Test", "sentence", "please", "ignore", "."], 1, 3) == Enum.filter(output, &(length(&1) <= 3))
+    assert Tokens.edge_shingles(["Test", "sentence", "please", "ignore", "."], 3) == Enum.filter(output, &(length(&1) >= 3))
+  end
+  
+  test "shingles" do
+    output = [
+      ["1", "2", "3"], ["2", "3", "4"], ["1", "2"], ["2", "3"], ["3", "4"]
+    ]
+    assert Tokens.shingles(["1", "2", "3", "4"]) == output
   end
 end

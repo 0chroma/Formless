8e6410ea6afc53df49933a23bb3596c5469f2be5
0chroma <21014+0chroma@users.noreply.github.com>
2016-03-25 20:51:46 -0700

    Added extra reference to treebank tokenizer

--- lib/formless/analysis/tokenizer/treebank.ex
+++ lib/formless/analysis/tokenizer/treebank.ex
@@ -1,4 +1,5 @@
 defmodule Formless.Analysis.Tokenizer.Treebank do
   # Based on treebank style of tokenization
   # https://www.cis.upenn.edu/~treebank/tokenization.html
+  # https://github.com/nltk/nltk/blob/develop/nltk/tokenize/treebank.py
 end

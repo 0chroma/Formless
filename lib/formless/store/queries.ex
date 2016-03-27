f875d87d93e4464d890084a6fd9f7da1ef6d4c3a
Will Riley <will@hflw.org>
2016-03-26 22:15:37 -0700

    Removed old quad generation code and started work on traversals

--- lib/formless/store/queries.ex
+++ lib/formless/store/queries.ex
@@ -39,23 +39,9 @@ defmodule Formless.Store.Queries do
   
   def traversal(seed, overlap=3) do
     """
-    var cursor = g.V("whenever anyone asks what the").ToValue();
-    g.Emit(cursor);
-    var get_random = function(result){
-      var index = Math.floor(Math.random()*result.length);
-      return result[index];
-    }
-    var find_next = function(id){
-      for(var i=2; i>=1; i--){
-        ret = get_random(g.V(id).LabelContext('twitter2').Out('ends_with_'+i).In('begins_with_'+i).ToArray());
-        if(ret && ret != id)
-          return ret;
-      }
-    }
-    for(var i=0; i<13 && cursor; i++){
-      cursor = find_next(cursor);
-      g.Emit(cursor);
-    }
+    MATCH p=(n:Shingle)-[:LEADS*1..5]->(m:Shingle)
+    WHERE "mybucket" in n.bucket
+    RETURN p
     """
   end
 end

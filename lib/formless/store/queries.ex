64e59dc9f6feaef3db6e3fa818fab3924f89af08
0chroma <21014+0chroma@users.noreply.github.com>
2016-03-26 21:50:44 -0700

    Added bucket functionality back in, now uses arrays for cases when a shingle was read from multiple sources

--- lib/formless/store/queries.ex
+++ lib/formless/store/queries.ex
@@ -1,28 +1,40 @@
 defmodule Formless.Store.Queries do
-  def shingle_to_cypher({shingle, side, subshingles}) do
+  def shingle_to_cypher({shingle, side, subshingles}, bucket) do
     subshingles
-    |> Enum.map(&shingle_subshingle_relationship(shingle, side, &1))
+    |> Enum.map(&shingle_subshingle_relationship(shingle, side, &1, bucket))
   end
-  defp shingle_subshingle_relationship(shingle, side, subshingle) do
+  defp shingle_subshingle_relationship(shingle, side, subshingle, bucket) do
     predicate = case side do
       :beginning -> "ENDS_WITH"
       :end -> "BEGINS_WITH"
     end
     """
       MERGE (n:Shingle #{node_props(shingle)})
+      #{bucket_property("n", bucket)}
       MERGE (m:Shingle #{node_props(subshingle)})
-      MERGE (n)-[:#{predicate}]->(m)
+      #{bucket_property("m", bucket)}
+      MERGE (n)-[r1:#{predicate}]->(m)
+      #{bucket_property("r1", bucket)}
       WITH m
       MATCH (s:Shingle)-[:ENDS_WITH]->(m)<-[:BEGINS_WITH]-(t:Shingle)
-      MERGE (s)-[:LEADS]->(t)
-      MERGE (s)<-[:FOLLOWS]-(t)
+      MERGE (s)-[r2:LEADS]->(t)
+      #{bucket_property("r2", bucket)}
+      MERGE (s)<-[r3:FOLLOWS]-(t)
+      #{bucket_property("r3", bucket)}
+    """
+  end
+  defp bucket_property(ref, bucket) do
+    bucket_escaped = Poison.encode! bucket
+    """
+      ON CREATE SET #{ref}.bucket=[#{bucket_escaped}]
+      ON MATCH SET #{ref}.bucket = FILTER(x in #{ref}.bucket WHERE NOT(x=#{bucket_escaped})) + #{bucket_escaped}
     """
   end
   defp node_props(shingle) do
     text = Enum.join(shingle)
-    textEscaped = Poison.encode! text
-    numTokens = length(shingle)
-    "{text: #{textEscaped}, numTokens: #{numTokens}, length: #{String.length(text)}}"
+    text_escaped = Poison.encode! text
+    num_tokens = length(shingle)
+    "{text: #{text_escaped}, numTokens: #{num_tokens}, length: #{String.length(text)}}"
   end
   
   def traversal(seed, overlap=3) do

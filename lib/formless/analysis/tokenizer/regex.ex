a020a4c066aee79d5f201176872f0c80bf3d9309
Will Riley <will@hflw.org>
2016-03-26 20:57:49 -0700

    Remplemented indexing logic

--- lib/formless/analysis/tokenizer/regex.ex
+++ lib/formless/analysis/tokenizer/regex.ex
@@ -1,7 +1,7 @@
 defmodule Formless.Analysis.Tokenizer.Regex do
   @behaviour Formless.Analysis.Tokenizer
 
-  @token_pattern ~r/\s|(?<=(\p{Po}|\p{Ps}))|(?=(\p{Po}|\p{Pe}))/u
+  @token_pattern ~r/(?<=(\p{Po}|\p{Ps}|\s))|(?=(\p{Po}|\p{Pe}|\s))/u
   def tokenize(text, opts \\ []) do
     regex = Keyword.get(opts, :regex, @token_pattern)
     String.split(text, regex, trim: true)

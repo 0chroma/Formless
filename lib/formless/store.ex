996d23343bcbe73cf8919f507924d2fe8e2c845b
Will Riley <will@hflw.org>
2016-03-26 21:50:44 -0700

    Added bucket functionality back in, now uses arrays for cases when a shingle was read from multiple sources

--- lib/formless/store.ex
+++ lib/formless/store.ex
@@ -8,17 +8,17 @@ defmodule Formless.Store do
     |> Tokens.sentences_from_text() # list of sentences
     |> Enum.map(&Tokens.words_from_text(&1)) # list of tokenized sentences
     |> Enum.flat_map(&shingles(&1)) # list of {shingle, side, [subshingles]} from each sentence
-    |> Enum.flat_map(&Queries.shingle_to_cypher(&1)) # list of cypher query strings
+    |> Enum.flat_map(&Queries.shingle_to_cypher(&1, bucket)) # list of cypher query strings
     |> write_to_neo4j()
   end
 
   defp shingles(tokens) do
     Enum.flat_map [:beginning, :end], fn(side) ->
-      Tokens.edge_shingles(tokens, side, 3)
+      Tokens.edge_shingles(tokens, side, 5)
       |> Enum.map(&subshingles(&1, side))
     end
   end
-  defp subshingles(tokens, side), do: {tokens, side, Tokens.edge_shingles(tokens, opposite_side(side))}
+  defp subshingles(tokens, side), do: {tokens, side, Tokens.edge_shingles(tokens, opposite_side(side), 2)}
   defp opposite_side(:beginning), do: :end
   defp opposite_side(:end), do: :beginning
 

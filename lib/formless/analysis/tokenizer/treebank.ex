073f8e0c4274d1091ef24429cceb9a2c1a78ea50
Will Riley <will@hflw.org>
2016-04-03 01:13:16 -0700

    Added performance todo

--- lib/formless/analysis/tokenizer/treebank.ex
+++ lib/formless/analysis/tokenizer/treebank.ex
@@ -2,4 +2,5 @@ defmodule Formless.Analysis.Tokenizer.Treebank do
   # Based on treebank style of tokenization
   # https://www.cis.upenn.edu/~treebank/tokenization.html
   # https://github.com/nltk/nltk/blob/develop/nltk/tokenize/treebank.py
+  # TODO: implement this next; being able to cut whitespace from token lists should make indexing twice as fast
 end
